6362b4a367a806b957604d9204e8a063d3fdc3c3
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2021-05-07 13:12:49 -0700

    Add ttbar PowhegHerwig sample

--- datasets.py
+++ datasets.py
@@ -1,3 +1,5 @@
 datasets = {
-    "ttbar" : "user.mromano.410470.PhPy8EG.DAOD_TOPQ1.e6337_s3126_r10724_p4031.TTDIFFXS344_v03.MINI359_KLFitter_v1_"
+    "ttbar" : "user.mromano.410470.PhPy8EG.DAOD_TOPQ1.e6337_s3126_r10724_p4031.TTDIFFXS344_v03.MINI359_KLFitter_v1_",
+    "ttbar_hw_lj"  : "user.mromano.410557.PowhegHerwig7EvtGen.DAOD_TOPQ1.e6366_a875_r10724_p4031.TTDIFFXS344_v02.MINI359_KLFitter_v1_",
+    "ttbar_hw_dil" : "user.mromano.410558.PowhegHerwig7EvtGen.DAOD_TOPQ1.e6366_a875_r10724_p4031.TTDIFFXS344_v02.MINI359_KLFitter_v1_"
 }
