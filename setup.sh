09a8e4bda879d3c4218401ebe73e8d37045eb175
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2021-06-27 14:16:06 -0700

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+# setupATLAS
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+
+lcgVersion=LCG_97apython3
+arch=x86_64-centos7-gcc8-opt
+lsetup "views $lcgVersion $arch"
+#source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh ${lcgVersion} ${arch}
+
+# lsetup rucio
+# NOTE: rucio client is only needed for listing input files
+# It messes up the environment for reading remote files via xrootd
+# Type "letup rucio" manually when generating job files or listing inputs
+# DO NOT set up rucio during actual running
+# unless all inputs are on local storage and xrootd is not needed
+
+lsetup xcache xrootd
+
 export SourceDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # cf. https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-
-lcgVersion=LCG_98python3_ATLAS_10
-arch=x86_64-centos7-gcc8-opt
-
-source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh ${lcgVersion} ${arch}
 
 export PYTHONPATH=$SourceDIR/python:$SourceDIR:$PYTHONPATH
