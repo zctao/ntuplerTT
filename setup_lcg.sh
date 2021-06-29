991c12d83d7569239e0fc77ff3a068d5ad5146bd
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2021-06-28 21:09:43 -0700

    Rearrange setup scripts

---
+++ setup_lcg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+lcgVersion=LCG_100
+arch=x86_64-centos7-gcc8-opt
+source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh ${lcgVersion} ${arch}
+
+export SourceDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# cf. https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+
+export PYTHONPATH=$SourceDIR/python:$SourceDIR:$PYTHONPATH
