91cbfb28513d32196cea0937b629f4771a5dd18b
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2021-07-15 16:01:36 -0700

    print host name in setup scripts

--- setup_atlas.sh
+++ setup_atlas.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+hostname=$(uname -n)
+echo $hostname
+
 # setupATLAS
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
