062542de9b39922c8bf07fd3bb216410433ed1ad
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2022-12-12 17:04:04 -0800

    Check job logs

--- scripts/checkOutputs.py
+++ scripts/checkOutputs.py
@@ -2,6 +2,65 @@ import os
 import yaml
 import ROOT
 
+def checkNumInputs(dirname):
+    inlistdir = os.path.join(dirname, "inputs")
+    print(f"Check input files in {inlistdir}")
+
+    indices = set()
+
+    for fname in os.listdir(inlistdir):
+        # get the index from the file name
+        index_str = os.path.splitext(fname)[0].split('_')[-1]
+        indices.add(index_str)
+
+    # return the number of indices
+    return len(indices)
+
+def checkJobLogs(dirname):
+    # Get the expected number of files from the number of input lists
+    njobs_exp = checkNumInputs(dirname)
+    njobs_success = 0
+
+    jobid = None
+
+    # Check the job logs
+    for fname in os.listdir(dirname):
+        # expect log extension '.out'
+        fext = os.path.splitext(fname)[-1]
+        if fext != '.out':
+            continue
+
+        # log name is the job id: 1234_5.out
+        jid = os.path.splitext(fname)[0]
+        jid = int(jid.split('_')[0])
+        if jobid is None:
+            jobid = jid
+        else:
+            if jid > jobid:
+                # This is a newer job log with larger id.
+                jobid = jid
+                # reset previous counts
+                njobs_success = 0
+            elif jid < jobid:
+                # This is an old job log. Skip
+                continue
+
+        # open the log file and read the last line
+        # exit code
+        logname = os.path.join(dirname, fname)
+        with open(logname, 'r') as flog:
+            last_line = flog.readlines()[-1]
+
+        if not "exit code " in last_line:
+            print(f"ERROR: cannot find the exit code in the last line of the log {logname}")
+        else:
+            # extract the exit code
+            exit_code = int(last_line.split('exit code ')[1])
+            if exit_code == 0:
+                njobs_success += 1
+
+    return str(njobs_success)+'/'+str(njobs_exp)
+
 def checkROOTinDir(dirname):
     print(f"Check ROOT files in {dirname}")
     nfiles = 0
@@ -12,6 +71,9 @@ def checkROOTinDir(dirname):
             continue
 
         if fname.endswith("_acc.root") or fname.endswith("_eff.root"):
+            continue
+
+        if fname.endswith("_histograms.root"):
             continue
 
         # found a ROOT file
@@ -40,7 +102,8 @@ def checkROOTinDir(dirname):
         except Exception as e:
             print(f"Failed to open ROOT file {fullname}: {e}")
 
-        ngood += int(ngoodtrees / len(treenames))
+        if treenames:
+            ngood += int(ngoodtrees / len(treenames))
 
     return str(ngood)+'/'+str(nfiles)
 
@@ -60,8 +123,11 @@ def checkOutputs(jDict, sDict):
             # check files
             res = checkROOTinDir(dirname)
 
+            # check logs
+            logs = checkJobLogs(dirname)
+
             # write to the result dictionary
-            oDict[k] = res
+            oDict[k] = f"{res} (files) {logs} (jobs)"
 
     return oDict
 
