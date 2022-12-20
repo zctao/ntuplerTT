094cbb5009e1d0befd8c253abf448d5bc40382c3
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2022-12-19 20:58:19 -0800

    Adjust job submission script

--- test/submit_mini362_v1.sh
+++ test/submit_mini362_v1.sh
@@ -22,21 +22,26 @@ python scripts/submitJobs.py ${jobcfg} -c systCRL -s VV Wjets Zjets singleTop tt
 #
 # detNP (detector systematic NP)
 systs="CategoryReduction_JET_"
-python scripts/submitJobs.py ${jobcfg} -c detNP -s ttbar VV singleTop ttH ttV -u ${systs}
+python scripts/submitJobs.py ${jobcfg} -c detNP -s ttbar -u ${systs}
+python scripts/submitJobs.py ${jobcfg} -c detNP -s VV singleTop ttH ttV -u ${systs}
 
 systs="EG_"
-python scripts/submitJobs.py ${jobcfg} -c detNP -s ttbar VV singleTop ttH ttV -u ${systs}
+python scripts/submitJobs.py ${jobcfg} -c detNP -s ttbar -u ${systs}
+python scripts/submitJobs.py ${jobcfg} -c detNP -s VV singleTop ttH ttV -u ${systs}
 
 systs="MET_"
-python scripts/submitJobs.py ${jobcfg} -c detNP -s ttbar VV singleTop ttH ttV -u ${systs}
+python scripts/submitJobs.py ${jobcfg} -c detNP -s ttbar -u ${systs}
+python scripts/submitJobs.py ${jobcfg} -c detNP -s VV singleTop ttH ttV -u ${systs}
 
 systs="MUON_"
-python scripts/submitJobs.py ${jobcfg} -c detNP -s ttbar VV singleTop ttH ttV -u ${systs}
+python scripts/submitJobs.py ${jobcfg} -c detNP -s ttbar -u ${systs}
+python scripts/submitJobs.py ${jobcfg} -c detNP -s VV singleTop ttH ttV -u ${systs}
 
 #
 # mcWAlt (MC weights and alternative samples)
-python scripts/submitJobs.py ${jobcfg} -c mcWAlt -s ttbar ttbar_hw
-python scripts/submitJobs.py ${jobcfg} -c mcWAlt -s ttbar_amc ttbar_hdamp ttbar_mt169 ttbar_mt176 ttbar_sh
+python scripts/submitJobs.py ${jobcfg} -c mcWAlt -s ttbar
+python scripts/submitJobs.py ${jobcfg} -c mcWAlt -s ttbar_hw ttbar_sh ttbar_amc
+python scripts/submitJobs.py ${jobcfg} -c mcWAlt -s ttbar_hdamp ttbar_mt169 ttbar_mt176
 python scripts/submitJobs.py ${jobcfg} -c mcWAlt -s VV VV_syst
 python scripts/submitJobs.py ${jobcfg} -c mcWAlt -s singleTop singleTop_DS singleTop_amc singleTop_hw
 python scripts/submitJobs.py ${jobcfg} -c mcWAlt -s ttH ttV ttV_syst
